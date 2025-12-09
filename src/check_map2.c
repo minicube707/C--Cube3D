0bccfc2ee06e127f19763f9f3dd1f95de9e5d9f0
Florent <florentm.contact@gmail.com>
2025-12-09 15:52:05 +0100

    First commit

--- src/check_map2.c
+++ src/check_map2.c
@@ -6,11 +6,40 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 14:43:04 by fmotte            #+#    #+#             */
-/*   Updated: 2025/12/08 14:43:24 by fmotte           ###   ########.fr       */
+/*   Updated: 2025/12/09 15:44:09 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	get_ori_player(char **tab_map)
+{
+	int		i;
+	int		j;
+	t_coord	coord;
+
+	i = 0;
+	coord.x = 0;
+	coord.y = 0;
+	while (tab_map[i] != NULL)
+	{
+		j = 0;
+		while (tab_map[i][j] != '\0')
+		{
+			if (tab_map[i][j] == 'N')
+				return (3);
+			if (tab_map[i][j] == 'S')
+				return (1);
+			if (tab_map[i][j] == 'E')
+				return (0);
+			if (tab_map[i][j] == 'W')
+				return (2);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 t_coord	get_pos_player(char **tab_map)
 {
