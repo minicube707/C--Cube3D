63e49220a38cc722e608264950500f5264281a6a
Florent <florentm.contact@gmail.com>
2026-05-16 16:43:43 +0200

    Correction new compilateur

--- src/graphic/main.c
+++ src/graphic/main.c
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 11:12:30 by cpollock          #+#    #+#             */
-/*   Updated: 2025/12/13 13:55:43 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/05/16 16:22:12 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ int	main(int argc, char **argv)
 	if (!init_game(&data))
 		return (kill_game(&data), 1);
 	init_player(&data.player);
-	mlx_hook(data.win, DestroyNotify, StructureNotifyMask, &close_game, &data);
-	mlx_hook(data.win, KeyPress, KeyPressMask, &key_press, &data);
-	mlx_hook(data.win, KeyRelease, KeyReleaseMask, &key_release, &data);
-	mlx_loop_hook(data.mlx, loop_event, &data);
+	mlx_hook(data.win, DestroyNotify, StructureNotifyMask, (int (*)(void))(intptr_t)close_game, &data);
+	mlx_hook(data.win, KeyPress, KeyPressMask, (int (*)(void))(intptr_t)key_press, &data);
+	mlx_hook(data.win, KeyRelease, KeyReleaseMask, (int (*)(void))(intptr_t)key_release, &data);
+	mlx_loop_hook(data.mlx, (int (*)(void))(intptr_t)loop_event, &data);
 	mlx_loop(data.mlx);
 	return (0);
 }
